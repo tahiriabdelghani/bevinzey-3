af62662843ce47f56b41eda770981862e160c5b6
hicham-stf1 <hzaydi78@gmail.com>
2023-04-15 17:48:41 +0000

    coupon added

--- src/pages/CouponFormPage.jsx
+++ src/pages/CouponFormPage.jsx
@@ -25,9 +25,8 @@ export default function CouponFormPage() {
         dispatch(clearMessage())
     }, [])
     const onsubmit = async () => {
+        setIsVisible(true);
         if (isCoupon) {
-            setIsVisible(true);
-
             await axios.post("https://plankton-app-q74hx.ondigitalocean.app/payment/subscription/initial",
                 {
                     plan: clientsecret?.plan,
@@ -79,104 +78,102 @@ export default function CouponFormPage() {
                         dispatch(clearMessage());
                     }, 5000);
                 })
-                :
-    navigate("/signin");
 
-    setIsVisible(false);
-} else {
-    await axios
-        .post(
-            "https://plankton-app-q74hx.ondigitalocean.app/payment/subscription/initial",
-            {
-                plan: clientsecret?.plan,
-                frequency: clientsecret?.frequency,
-                email: user?.email
-            }
-        )
-        .then((res) => {
-            navigate("/payment");
-            dispatch(
-                setClientSecret({
-                    ...clientsecret,
-                    clientsecret: res?.data.client_secret,
-                    priceReduction: { amount: null, percentage: null }
+        } else {
+            await axios
+                .post(
+                    "https://plankton-app-q74hx.ondigitalocean.app/payment/subscription/initial",
+                    {
+                        plan: clientsecret?.plan,
+                        frequency: clientsecret?.frequency,
+                        email: user?.email
+                    }
+                )
+                .then((res) => {
+                    navigate("/payment");
+                    dispatch(
+                        setClientSecret({
+                            ...clientsecret,
+                            clientsecret: res?.data.client_secret,
+                            priceReduction: { amount: null, percentage: null }
+                        })
+                    );
                 })
-            );
-        })
-        .catch((error) => {
-            dispatch(
-                setMessage(
-                    error.response &&
-                    error.response.data &&
-                    error.response.data.message
-                ) ||
-                error.message ||
-                error.toString()
-            );
-            setTimeout(() => {
-                dispatch(clearMessage());
-            }, 5000);
-        })
-}
+                .catch((error) => {
+                    dispatch(
+                        setMessage(
+                            error.response &&
+                            error.response.data &&
+                            error.response.data.message
+                        ) ||
+                        error.message ||
+                        error.toString()
+                    );
+                    setTimeout(() => {
+                        dispatch(clearMessage());
+                    }, 5000);
+                })
+        }
+        setIsVisible(false);
     }
 
-return (
-    <div className='flex  h-screen justify-center items-center flex-col'>
-        <p className='text-md my-6 font-medium text-white'>Enter Your Coupon Code for Extra Discounts on Your Purchase</p>
+    return (
+        <div className='flex  h-screen justify-center items-center flex-col'>
+            <p className='text-md my-6 font-medium text-white'>Enter Your Coupon Code for Extra Discounts on Your Purchase</p>
 
-        <div className='bg-white shadow-xl h-64 w-[90%] md:w-[40%] rounded-lg flex flex-col'>
-            <div onClick={() => navigate('/')} className='cursor-pointer flex justify-end pt-1 pr-1  items-end text-end'>
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff4500" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <circle cx="12" cy="12" r="9" />
-                    <path d="M10 10l4 4m0 -4l-4 4" />
-                </svg>
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-                <CoupounForm isCoupon={isCoupon} handleChange={handleChange} />
-                {isCoupon &&
-                    <div className='flex flex-col mt-2'>
-                        <label className="text-gray-600">
-                            Coupon Code
-                        </label>
-                        <input
-                            className="text-gray-700 my-2 w-60"
-                            type="text"
-                            placeholder="Enter coupon code"
-                            value={couponCode}
-                            onChange={(e) => setCouponCode(e.target.value)}
-                        /></div>}
+            <div className='bg-white shadow-xl h-64 w-[90%] md:w-[40%] rounded-lg flex flex-col'>
+                <div onClick={() => navigate('/')} className='cursor-pointer flex justify-end pt-1 pr-1  items-end text-end'>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff4500" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="12" cy="12" r="9" />
+                        <path d="M10 10l4 4m0 -4l-4 4" />
+                    </svg>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <CoupounForm isCoupon={isCoupon} handleChange={handleChange} />
+                    {isCoupon &&
+                        <div className='flex flex-col mt-2'>
+                            <label className="text-gray-600">
+                                Coupon Code
+                            </label>
+                            <input
+                                className="text-gray-700 my-2 w-60"
+                                type="text"
+                                placeholder="Enter coupon code"
+                                value={couponCode}
+                                onChange={(e) => setCouponCode(e.target.value)}
+                            /></div>}
 
-                {visible &&
-                    <div className="z-50 absolute top-[50%] left-[50%] -translate-x-[50%]">
-                        <ColorRing visible={true}
-                            height="100"
-                            width="100"
-                            ariaLabel="blocks-loading"
-                            wrapperStyle={{}}
-                            wrapperClass="blocks-wrapper"
-                            colors={['#164bd3', '#126eba', '#1850b1', '#5869c9', '#132a77']}
-                        /></div>
-                }
+                    {visible &&
+                        <div className="z-50 absolute top-[50%] left-[50%] -translate-x-[50%]">
+                            <ColorRing visible={true}
+                                height="100"
+                                width="100"
+                                ariaLabel="blocks-loading"
+                                wrapperStyle={{}}
+                                wrapperClass="blocks-wrapper"
+                                colors={['#164bd3', '#126eba', '#1850b1', '#5869c9', '#132a77']}
+                            /></div>
+                    }
 
-                <p className=" mx-3 text-xs font-bold text-center text-green-500 ">
-                    {message && (
-                        <div
-                            className="text-red-500"
-                            role="alert"
-                        >
-                            {message}
-                        </div>
-                    )}
-                </p>
-                <button className="w-[28%] mx-[26%] flex justify-center items-center 
+                    <p className=" mx-3 text-xs font-bold text-center text-green-500 ">
+                        {message && (
+                            <div
+                                className="text-red-500"
+                                role="alert"
+                            >
+                                {message}
+                            </div>
+                        )}
+                    </p>
+                    <button className="w-[28%] mx-[26%] flex justify-center items-center 
       bg-gradient-to-r from-orange-500 via-orange-600 to-orange-500 font-bold
       py-2.5 my-2" onClick={onsubmit} >
-                    {isCoupon ? "Apply Coupon" : "Skip"}
-                </button>
-            </div>
+                        {isCoupon ? "Apply Coupon" : "Skip"}
+                    </button>
+                </div>
 
+            </div>
         </div>
-    </div>
-)
+    )
 }

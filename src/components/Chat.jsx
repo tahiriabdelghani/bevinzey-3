d265562a8d9979ebe0da7d058ed5b6fbd79a6c63
tahiriabdelghani <tahiriabdelghani111@gmail.com>
2023-04-16 17:52:11 +0000

    chat bug, respponsivity bug

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsFillTrashFill } from "react-icons/bs";
+import { CgClose } from "react-icons/cg";
+import { AiOutlineCheck } from "react-icons/ai";
+import axios from "axios";
 
-function Chat({ chat, selectedChat, onClick }) {
+function Chat({ chat, selectedChat, onClick, getChats, setSelectedChat,setMessages }) {
+  const [deleteClicked, setDeleteClicked] = useState(false);
+
+  const deleteChat = () => {
+    axios
+      .get(
+        "https://plankton-app-q74hx.ondigitalocean.app/ai-services/delete/chat/" +
+          chat.id
+      )
+      .then((res) => {
+        getChats();
+        setSelectedChat(null)
+        setMessages([])
+      });
+  };
   return (
     <div
       onClick={onClick}
-      className={`text-gray-500 ml-2 text-sm p-3 border-b-2 border-gray-300 cursor-pointer ${
+      className={`relative max-w-[280px] truncate text-gray-500 ml-2 text-sm p-3 pl-1 border-b-2 border-gray-300 cursor-pointer ${
         selectedChat === chat.id && "bg-gray-100"
       }`}
     >
-      {chat.title ?? "New chat"}
+      {selectedChat === chat.id && !deleteClicked && (
+        <div
+          onClick={() => {
+            setDeleteClicked(true);
+          }}
+          className="absolute right-3 bottom-3 p-1 rounded flex bg-red-200"
+        >
+          <BsFillTrashFill className="text-[#9997fe]" size={14} />
+        </div>
+      )}
+      {selectedChat === chat.id && deleteClicked && (
+        <div className="absolute right-3 bottom-3  rounded flex">
+          <div className=" p-1 rounded flex bg-red-200 mr-1">
+            <AiOutlineCheck onClick={deleteChat} className="text-[#9997fe]" size={14} />
+          </div>
+          <div
+            onClick={() => {
+              setDeleteClicked(false);
+            }}
+            className=" p-1 rounded flex bg-green-200"
+          >
+            <CgClose className="text-[#9997fe]" size={14} />
+          </div>
+        </div>
+      )}
+      {chat.title ?? "New chat"}{" "}
     </div>
   );
 }
--- src/components/messages/BotMessage.jsx
+++ src/components/messages/BotMessage.jsx
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdOutlineContentCopy } from "react-icons/md";
 
 function BotMessage({ text }) {
+  const handleClick = (event) => {
+    switch (event.detail) {
+      case 1: {
+        break;
+      }
+      case 2: {
+        copyScript();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  const [copied, setCopied] = useState(false);
+  const copyScript = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
   return (
-    <div className="flex items-start my-4 last:mb-0">
+    <div
+      onClick={handleClick}
+      className="flex cursor-pointer items-start my-4 last:mb-0 select-none"
+    >
       <img
         className="rounded-full mr-4"
         src="/images/bevinzey-bot.png"
         width="40"
         height="40"
+        alt="User 01"
       />
-      <div className="">
-        <pre className="text-sm font-inter whitespace-pre-wrap bg-white text-slate-800 p-3 rounded-lg rounded-tl-none border border-slate-200 shadow-md mb-1">
-          {text}
-        </pre>
+      <div className="relative flex items-center">
+        <div>
+          <pre className="text-sm font-inter whitespace-pre-wrap bg-white text-slate-800 p-3 rounded-lg rounded-tl-none border border-slate-200 shadow-md mb-1">
+            {text}
+          </pre>
+        </div>
+        {copied && (
+          <span className="text-[11px] absolute -right-[25px] text-white bg-[#8886fe] rounded p-1 mb-1">
+          <MdOutlineContentCopy size={12}/>
+        </span>
+        )}
       </div>
     </div>
   );
